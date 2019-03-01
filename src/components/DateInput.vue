51ccdf696c5515cce25c4db2f09ebb7669767e4f
Dusan Aleksic <aledujke@gmail.com>
2019-03-01 16:06:28 +0100

    Use prop function instead Date.parse

--- src/components/DateInput.vue
+++ src/components/DateInput.vue
@@ -45,7 +45,10 @@ export default {
     selectedDate: Date,
     resetTypedDate: [Date],
     format: [String, Function],
-    inputFormat: Function,
+    inputFormat: {
+      type: Function,
+      default: Date.parse
+    },
     translation: Object,
     inline: Boolean,
     id: String,
@@ -120,7 +123,6 @@ export default {
 
       if (this.typeable) {
         const typedDate = this.inputFormat(this.input.value)
-        debugger
         if (!isNaN(typedDate)) {
           this.typedDate = this.input.value
           this.$emit('typedDate', new Date(typedDate))
@@ -133,7 +135,7 @@ export default {
      * called once the input is blurred
      */
     inputBlurred () {
-      if (this.typeable && isNaN(Date.parse(this.input.value))) {
+      if (this.typeable && isNaN(this.inputFormat(this.input.value))) {
         this.clearDate()
         this.input.value = null
         this.typedDate = null
